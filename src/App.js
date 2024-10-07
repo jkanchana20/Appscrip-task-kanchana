8cc4a9480cd249ac2b8622b2b1d5d82a06b0f9a1
jkanchana20 <160008195+jkanchana20@users.noreply.github.com>
2024-10-07 13:19:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Hero from "../src/components/Hero"
+import Home from "../src/components/Home"
 function App() {
   return (
     <div className="App">
-     <Hero/>
+     <Home/>
     </div>
   );
 }
